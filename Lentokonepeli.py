46b734dbbb29d1c06e40bc4976b5eda9280f43fe
Nelos123 <pecizejnel@gmail.com>
2026-02-16 01:57:49 +0200

    Update Lentokonepeli.py

--- Lentokonepeli.py
+++ Lentokonepeli.py
@@ -6,3 +6,13 @@ class Airport:
     def init(self, name, country_code):
         self.name = name
         self.country_code = country_code
+
+class Game:
+    def init(self):
+        self.airports = []
+        self.countries = {}
+        self.remaining_countries = set()
+        self.correct_answers = 0
+        self.total_questions = 0
+        self.load_country_data()
+        self.load_airports_from_csv()
